bae5aec088007aedb72ea8bec0d334d3bf92f6ac
sid-r-singh <68782815+sid-r-singh@users.noreply.github.com>
2021-03-16 11:43:02 +0530

    Update fresh_bot.py

--- fresh_bot.py
+++ fresh_bot.py
@@ -128,7 +128,7 @@ def main():
     # log all errors
     dp.add_error_handler(error)
 	
-
+    PORT = int(os.environ.get("PORT", "5000"))
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
     updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
     #updater.bot.setWebhook('https://YOURHEROKUAPPNAME.herokuapp.com/' + TOKEN)
